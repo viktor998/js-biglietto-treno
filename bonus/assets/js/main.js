f13eed295194f4d10a8f8d961a6bbe018e043f90
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-03-01 17:58:10 +0100

    bonus terminato

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,6 +1,27 @@
+var kilometri =prompt("Quanti chilometri deve percorrere?:");
+var eta = parseInt(prompt("Quale è la sua età:"));
+var k = 0.21;
+var prezzoBiglietto = kilometri * k;
+var totale = document.getElementById("tot");
+
+console.log(kilometri, eta);
+
+if(eta < 18){
+    console.log("il prezzo del biglietto è :" + (prezzoBiglietto - ((prezzoBiglietto * 20) / 100)) + "$");
+    totale.innerHTML +=(prezzoBiglietto - ((prezzoBiglietto * 20) / 100)) + "$";
+}
+else if(eta > 65){
+    console.log("il prezzo del biglietto è :" + (prezzoBiglietto - ((prezzoBiglietto * 40) / 100)) + "$");
+}
+else{
+    console.log("il prezzo del biglietto è :" + prezzoBiglietto + "$");
+}
+
 var id = 0;
 function moveOn() {
   var elem = document.getElementById("train");
+  var icon= document.getElementById("iconPulse");
+  icon.style.opacity=0;
   var pos = 0;
   clearInterval(id);
   id = setInterval(frame, 1);
@@ -12,26 +33,4 @@ function moveOn() {
       elem.style.right = pos + 'px';
     }
   }
-}
-
-function biglietto(){
-    var kilometri = document.querySelector('#kilometri').value;
-    var eta = document.querySelector("#eta").value;
-    var k = 0.21;
-    var prezzoBiglietto = kilometri * k;
-    document.getElementById("myForm").submit();
-
-    if(eta < 18){
-        console.log("il prezzo del biglietto è :" + (prezzoBiglietto - ((prezzoBiglietto * 20) / 100)) + "$");
-        document.getElementById("totale").value =(prezzoBiglietto - ((prezzoBiglietto * 20) / 100));
-    }
-    else if(eta > 65){
-        console.log("il prezzo del biglietto è :" + (prezzoBiglietto - ((prezzoBiglietto * 40) / 100)) + "$");
-        document.getElementById("totale").value =(prezzoBiglietto - ((prezzoBiglietto * 40) / 100));
-    }
-    else{
-        console.log("il prezzo del biglietto è :" + prezzoBiglietto + "$");
-        document.getElementById("totale").value =prezzoBiglietto ;
-    }
-
 }
